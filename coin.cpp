40022a095a6367efc163b958470767cfa3799928
Allen <allen20070115@gmail.com>
2026-05-31 23:46:04 +0800

    從儲存庫移除所有檔案的追蹤

--- coin.cpp
+++
@@ -1,9 +0,0 @@
-#include "coin.h"
-
-Coin::Coin() { value = 0; }
-
-Coin::Coin(int v) { value = v; }
-
-int Coin::getValue() const { return value; }
-
-void Coin::setValue(int v) { value = v; }
--- main.cpp
+++
@@ -1,7 +0,0 @@
-#include "game.h"
-
-int main() {
-    game blackjack;
-    blackjack.run();
-    return 0;
-}
